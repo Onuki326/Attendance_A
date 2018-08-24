6ea2023f092fc09c7980d5c3e6ddf0c20a721d00
Yuta Onuki <onu4648yu@gmail.com>
2018-08-24 00:02:58 +0000

    Complete signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,33 @@ class UsersController < ApplicationController
   def edit
   end
   
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to users_url("#")
+    else
+      render 'edit'
+    end  
+  end
+  
   def new
     @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功をここで扱う。
+      flash[:success] = "登録できました"
+    else
+      render 'new'
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+  
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -8,13 +8,24 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
   
   
-  test "should get edit" do
-    get edit_user_path(@user)
+  #test "should get edit" do
+    #get edit_user_path(@user)
+    #assert_response :success
+  #end
+  
+  test "sohuld get new" do
+    get signup_path
     assert_response :success
   end
   
-  test "sohuld get new" do
-    get new_user_path
-    assert_response :success
-  end  
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+  end
+  
 end
