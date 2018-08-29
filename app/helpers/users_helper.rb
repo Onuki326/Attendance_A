d8076a099b61b1175cc9351cbf811681491370fc
Yuta Onuki <onu4648yu@gmail.com>
2018-08-29 14:31:13 +0000

    Create method this_month

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  
+  def advance_month
+    @month = DateTime.now
+  end
+  
+  def this_month
+    @month = DateTime.now
+    @this_month = "#{@month.year}" + "年" + "#{@month.month}" + "月"
+  end
 end
