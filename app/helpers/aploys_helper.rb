7e9f261ef8bb0e16a03979c45a5c221c0a1a2989
Yuta Onuki <onu4648yu@icloud.com>
2018-11-28 14:03:32 +0000

    1ヶ月分勤怠申請機能完成

--- app/helpers/aploys_helper.rb
+++ app/helpers/aploys_helper.rb
@@ -1,7 +1,7 @@
 module AploysHelper
   
   def user_aploy(user)
-    aploy_user = user.aploys.where(user_id: user.id)
+    aploy_user = user.aploys.where(user_id: user.id, sperior_id: @user.id)
   end
   
   def aploy_month(aploy)
